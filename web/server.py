a6bd44f713d74cb32aa1bc0e82ba4ce60ab33383
Enrique Sobrados <39936196+Enrique720@users.noreply.github.com>
2019-06-08 16:22:59 -0500

    Add files via upload

--- web/server.py
+++ web/server.py
@@ -63,7 +63,6 @@ def create_user():
 
 @app.route('/authenticate', methods = ["POST"])
 def authenticate():
-    time.sleep(8)
     message = json.loads(request.data)
     username = message['username']
     password = message['password']
